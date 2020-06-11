5a40557c2effad28b82b728d93dc50cdd07d7134
Leigha Jarett <leighajarett@Leighas-MacBook-Pro-2.local>
2020-06-11 12:10:12 -0400

    Adding build

---
+++ src/Tile.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from 'react'
+import { Paragraph, Heading, Box, ButtonOutline, ComponentsProvider } from '@looker/components';
+import styled, { ThemeProvider } from 'styled-components'
+
+const InnerBox = styled(Box)`
+    box-shadow: 4px 5px 2px #ECECEC;
+    border-left: 1px solid #ECECEC;`
+
+const ButtonBox =  styled(Box)`
+    vertical-align: bottom;`
+
+// Create (or import) our react component
+export default function Tile (props) {
+  var [color, setColor] = React.useState("#ECECEC");
+  var [buttons, setButtons] = React.useState([]);
+  // var [detailLink, setLink] = React.useState(null);
+  // var [buttonTitles,setTitles] = React.useState(['Go to Dashboards','See Demo Details']);
+
+
+  useEffect(() => {
+    if (props.colorList) {
+      var current_color = props.colorList.filter(function (el) {
+        return el.value == props.tag });
+      if(current_color.length>0){
+        setColor(current_color[0].color)
+      }
+    }
+    else {
+      setColor("#ECECEC")
+    }
+  }, [props.colorList]);
+
+  useEffect(() => {
+    if (props.buttons) {
+      setButtons(props.buttons);
+    }
+    else{
+      setButtons([]);
+    }
+  }, [props.buttons]);
+
+  console.log(props.buttons)
+  return(
+      <InnerBox
+        paddingLeft={"20px"}
+        minWidth={"500px"}
+        width={"25%"}
+        margin={"10px"}
+        borderTop = {"5px solid " + color}
+        position={"relative"}
+        height={"auto"}
+      >
+      <Paragraph fontSize={'small'} fontFamily={'Open Sans'} marginTop={"10px"}>{props.tag}</Paragraph>
+      <Box marginTop={"30px"} marginBottom={"100px"} >
+          <Heading fontSize={"large"} fontFamily={'Open Sans'} fontWeight={'bold'}>{props.name}</Heading>
+          <Paragraph fontFamily={'Open Sans'} marginRight={"10px"} marginBottom={"20px"}>{props.description}</Paragraph>
+      </Box>
+      <Box position={"absolute"} bottom={0}>
+        <ComponentsProvider>
+          {
+            buttons.map((button, i) => 
+              button.link ?
+              <ButtonOutline key={i} color="neutral" size="small" iconBefore={button.icon} marginRight={"10px"} marginBottom={"10px"} href={button.link} target={"_blank"}>
+                {button.name}
+              </ButtonOutline>
+              :
+              <Paragraph></Paragraph>
+            )
+          }   
+        </ComponentsProvider>
+      </Box>
+      </InnerBox>
+  )
+}
